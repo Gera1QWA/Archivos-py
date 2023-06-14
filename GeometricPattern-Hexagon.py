cd8c098dea50b4bc95a853d7d71fda1671e51d5d
Gera1QWA <91513443+Gera1QWA@users.noreply.github.com>
2023-06-13 23:33:23 -0600

    archivos pyhton

---
+++ GeometricPattern-Hexagon.py
@@ -0,0 +1,14 @@
+# Programación creativa con Python
+# PATRONES CON HEXAGONOS
+#
+# Curso de Python
+# Primavera de 2023
+# 
+import turtle
+shelly = turtle.Turtle()
+for n in range(72):
+    for i in range(6):
+        shelly.forward(100)
+        shelly.left(60)
+    shelly.right(5)
+            
---
+++ Hola.py
@@ -0,0 +1,12 @@
+
+
+a = ' bienvenido al mundo de ...'
+b = ' Alfonso '
+c = ' Hola '
+d = ' PYTHON '
+print (" Hola Alfonso bienvenido al mundo de Python ")
+print (c+b+a+d)
+
+
+ 
+            
